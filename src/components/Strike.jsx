1a81b07178d1aec556808a7ba5e07ef0d2d78e90
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-03-17 19:28:56 +0530

    Strike Through component completed

--- src/components/Strike.jsx
+++ src/components/Strike.jsx
@@ -1,5 +1,5 @@
-const Strike = () => {
-  return <div></div>
+const Strike = ({ strikeClass }) => {
+  return <div className={`strike ${strikeClass}`}></div>
 }
 
 export default Strike
--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -7,6 +7,7 @@ const PLAYER_O = "O"
 const TicTacToe = () => {
   const [tiles, setTiles] = useState(Array(9).fill(null))
   const [playerTurn, setPlayerTurn] = useState(PLAYER_X)
+  const [strikeClass, setStrikeClass] = useState()
 
   const handleTileClick = index => {
     if (tiles[index] !== null) {
@@ -30,6 +31,7 @@ const TicTacToe = () => {
         playerTurn={playerTurn}
         tiles={tiles}
         onTileClick={handleTileClick}
+        strikeClass={strikeClass}
       />
     </div>
   )
